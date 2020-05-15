86373cc064efb3e6beb977017c0ec45aa528e2e4
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 00:04:23 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriach => {"Lord Montague"},
-      :matriarch => {"Lady Montague"},
-      :hero => {"Romeo"},
-      :hero_friends => [{"Benvolio"}, {"Mercutio"}]
-    :capulet => {
-      #your key/value pairs here!
-      :patriarch => {"Lord Capulet"},
-      :matriarch => {"Lady Capulet"},
-      :heroine => {"Juliet"},
-      :heroine_friends => [{"Benvolio"}, {"Mercutio"}]
-      }
-    }
+     :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+     }
+  }
 
 
 end
