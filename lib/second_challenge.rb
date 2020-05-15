17b72c5a4b00cf374e93f8f001c6aa08e0594a82
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 00:00:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,30 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriach => {"Lord Montague"},
+      :matriarch => {"Lady Montague"},
+      :hero => {"Romeo"},
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio"
+          :age => "18"
+          :attitude => "hot-headed"
+        }
+      ]
       }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+    }
 
   
 end
